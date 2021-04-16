212a1edb6fac4add85613568e198a0bace28dceb
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-16 09:43:13 -0300

    feat: adiciona classe ParadasConfiguration

---
+++ Observer.Application/Data/DataConfigurations/ParadasConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ObserverAPI.Entities;
+
+namespace ObserverAPI.Data.DataConfigurations
+{
+    public class ParadasConfiguration : IEntityTypeConfiguration<Parada>
+    {
+        public void Configure(EntityTypeBuilder<Parada> builder)
+        {
+            builder
+                .HasKey(p => p.Id);
+        }
+    }
+}
