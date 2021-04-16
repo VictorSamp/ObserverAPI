da22432991b0a07624e33ca2f11a059571005103
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-16 13:59:29 -0300

    feat: cria classe DTO para adicao de novo objeto Parada

---
+++ Observer.Application/Models/InputModels/AddParadaInputModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ObserverAPI.Models.InputModels
+{
+    public class AddParadaInputModel
+    {
+        public string Nome { get; private set; }
+        public double Latitude { get; private set; }
+        public double Longitude { get; private set; }
+
+        public AddParadaInputModel(string nome, double latitude, double longitude)
+        {
+            Nome = nome;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+    }
+}
