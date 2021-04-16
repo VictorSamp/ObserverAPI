a2c491436c8cbc6ad08d0168def0f298be818e66
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-16 09:32:20 -0300

    feat: cria entidade Parada

---
+++ Observer.Application/Entities/Parada.cs
@@ -0,0 +1,10 @@
+﻿namespace ObserverAPI.Entities
+{
+    public class Parada
+    {
+        public long Id { get; private set; }
+        public string Nome { get; private set; }
+        public double Latitude { get; private set; }
+        public double Longitude { get; private set; }
+    }
+}
