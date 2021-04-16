a1d25f0b6078887b83c0c03ebc5a9c70d08edbc9
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-16 13:49:06 -0300

    refactor: insere DTO nos metodos get da classe ParadasController

--- Observer.Application/Controllers/ParadasController.cs
+++ Observer.Application/Controllers/ParadasController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using ObserverAPI.Data;
 using ObserverAPI.Entities;
+using ObserverAPI.Models.ViewModels;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -20,25 +21,35 @@ namespace ObserverAPI.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Parada>>> GetAll()
+        public async Task<ActionResult<IEnumerable<ParadaViewModel>>> GetAll()
         {
             var paradas = await _context.Paradas.ToListAsync();
 
-            return Ok(paradas);
+            var paradaViewModel = paradas
+                .Select(p => new ParadaViewModel(p.Id, p.Nome, p.Latitude, p.Longitude))
+                .ToList();
+
+            return Ok(paradaViewModel);
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Parada>> GetById(long id)
+        public async Task<ActionResult<ParadaViewModel>> GetById(long id)
         {
             var parada = await _context.Paradas
-                .FindAsync(id);
+                .SingleOrDefaultAsync(p => p.Id == id);
 
             if (parada == null)
             {
                 return NotFound();
             }
 
-            return Ok(parada);
+            var paradaViewModel = new ParadaViewModel(
+                parada.Id,
+                parada.Nome,
+                parada.Latitude,
+                parada.Longitude);
+
+            return Ok(paradaViewModel);
         }
 
         [HttpPost]
---
+++ Observer.Application/Models/ViewModels/ParadaViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace ObserverAPI.Models.ViewModels
+{
+    public class ParadaViewModel
+    {
+        public long Id { get; private set; }
+        public string Nome { get; private set; }
+        public double Latitude { get; private set; }
+        public double Longitude { get; private set; }
+
+        public ParadaViewModel(long id, string nome, double latitude, double longitude)
+        {
+            Id = id;
+            Nome = nome;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+    }
+}
