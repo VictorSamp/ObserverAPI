0a853f9a5cc93d6cf5463f07a58cbcd0a388af36
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-16 09:40:39 -0300

    feat: configura classe ObserverDbContext e adiciona connection string

---
+++ Observer.Application/Data/ObserverDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ObserverAPI.Entities;
+using System.Reflection;
+
+namespace ObserverAPI.Data
+{
+    public class ObserverDbContext : DbContext
+    {
+        public ObserverDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Parada> Paradas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
