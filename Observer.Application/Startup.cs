72d8f21c0de96ad2ae29e47f1e5515d830e4e228
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-16 11:15:56 -0300

    feat: configura uso do  SqlServer na classe Startup

--- Observer.Application/Startup.cs
+++ Observer.Application/Startup.cs
@@ -1,15 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ObserverAPI.Data;
 
 namespace Observer.Application
 {
@@ -25,6 +21,10 @@ namespace Observer.Application
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration.GetConnectionString("ObserverAPI");
+
+            services.AddDbContext<ObserverDbContext>(
+                options => options.UseSqlServer(connectionString));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
