ec16ea1bdeef6ada149847318b739f36f0c643d0
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-19 09:54:17 -0300

    feat: cria classe Linha

---
+++ Observer.Application/Entities/Linha.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ObserverAPI.Entities
+{
+    public class Linha
+    {
+        public long Id { get; private set; }
+        public string Nome { get; private set; }
+        public List<Parada> Paradas { get; private set; }
+
+        public Linha(string nome)
+        {
+            Nome = nome;
+            Paradas = new List<Parada>();
+        }
+    }
+}
