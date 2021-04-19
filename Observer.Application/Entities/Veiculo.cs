f8649160c8242ffe2b336e3f1e10a95bbb107a32
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-19 15:34:22 -0300

    feat: cria classe Veiculo

---
+++ Observer.Application/Entities/Veiculo.cs
@@ -0,0 +1,10 @@
+﻿namespace ObserverAPI.Entities
+{
+    public class Veiculo
+    {
+        public long Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Modelo { get; private set; }
+        public Linha LinhaId { get; private set; }
+    }
+}
