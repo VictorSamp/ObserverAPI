3a4b48594481d732cdd58052b95f25b6441fd373
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-19 15:49:39 -0300

    feat: cria migration VeiculoMigration e atualiza banco de dados

--- Observer.Application/Data/Migrations/20210419184832_VeiculoMigration.Designer.cs
+++ Observer.Application/Data/Migrations/20210419184832_VeiculoMigration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ObserverAPI.Data;
 
 namespace ObserverAPI.Data.Migrations
 {
     [DbContext(typeof(ObserverDbContext))]
-    partial class ObserverDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210419184832_VeiculoMigration")]
+    partial class VeiculoMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Observer.Application/Data/Migrations/20210419184832_VeiculoMigration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ObserverAPI.Data.Migrations
+{
+    public partial class VeiculoMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Veiculos",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Modelo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LinhaId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Veiculos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Veiculos_Linhas_LinhaId",
+                        column: x => x.LinhaId,
+                        principalTable: "Linhas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Veiculos_LinhaId",
+                table: "Veiculos",
+                column: "LinhaId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Veiculos");
+        }
+    }
+}
