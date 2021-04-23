d9a5267ee3c9625106eccb11dd25db8ed4162307
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-23 13:26:37 -0300

    feat: adiciona na entidade Veiculo o construtor e o metodo UpdateVeiculo

--- Observer.Application/Entities/Veiculo.cs
+++ Observer.Application/Entities/Veiculo.cs
@@ -6,5 +6,17 @@
         public string Nome { get; private set; }
         public string Modelo { get; private set; }
         public long LinhaId { get; private set; }
+
+        public Veiculo(string nome, string modelo)
+        {
+            Nome = nome;
+            Modelo = modelo;
+        }
+
+        public void UpdateVeiculo(string nome, string modelo)
+        {
+            Nome = nome;
+            Modelo = modelo;
+        }
     }
 }
