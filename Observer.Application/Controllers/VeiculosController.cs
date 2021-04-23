be6b7ef5a17621746533b5b4cddfba48ab2fb780
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-23 13:25:29 -0300

    refactor: finaliza CRUD da entidade Veiculo

--- Observer.Application/Controllers/VeiculosController.cs
+++ Observer.Application/Controllers/VeiculosController.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ObserverAPI.Data;
+using ObserverAPI.Entities;
+using ObserverAPI.Models.InputModels;
+using ObserverAPI.Models.ViewModels;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ObserverAPI.Controllers
@@ -19,31 +24,73 @@ namespace ObserverAPI.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<VeiculoViewModel>>> GetAll()
         {
-            return Ok();
+            var veiculos = await _context.Veiculos.ToListAsync();
+
+            var veiculoViewModel = veiculos
+                .Select(v => new VeiculoViewModel(v.Id, v.Nome, v.Modelo, v.LinhaId))
+                .ToList();
+
+            return Ok(veiculoViewModel);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<VeiculoViewModel>> GetById(long id)
         {
-            return Ok();
+            var veiculo = await _context.Veiculos
+                .SingleOrDefaultAsync(v => v.Id == id);
+
+            if (veiculo == null)
+            {
+                return NotFound();
+            }
+
+            var veiculoViewModel = new VeiculoViewModel(veiculo.Id, veiculo.Nome, veiculo.Modelo, veiculo.LinhaId);
+
+            return Ok(veiculoViewModel);
         }
 
         [HttpPost]
-        public async Task<ActionResult<Veiculo>> Post([FromBody] AddVeiculoInputModel model)
+        public async Task<ActionResult> Post([FromBody] AddVeiculoInputModel model)
         {
-            return Ok();
+            var veiculo = new Veiculo(model.Nome, model.Modelo);
+
+            _context.Veiculos.Add(veiculo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                nameof(GetById),
+                new { id = veiculo.Id },
+                veiculo
+                );
         }
 
         [HttpPut("{id}")]
         public async Task<ActionResult> Put(long id, [FromBody] UpdateVeiculoInputModel model)
         {
-            return Ok();
+            var veiculo = await _context.Veiculos
+                .SingleOrDefaultAsync(v => v.Id == id);
+
+            if (veiculo == null)
+            {
+                return NotFound();
+            }
+
+            veiculo.UpdateVeiculo(model.Nome, model.Modelo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult> Delete(long id)
         {
-            return Ok();
+            var veiculo = await _context.Veiculos
+                .SingleOrDefaultAsync(v => v.Id == id);
+
+            _context.Veiculos.Remove(veiculo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
